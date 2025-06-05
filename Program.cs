ab98084d25c3186cec7170ff45def3c9864985e1
victorFeracin <victorhugo.feracin@gmail.com>
2025-06-04 21:11:37 -0300

    Resolucao do desafio dotnet POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("NOKIA:");
+Nokia nokia = new Nokia("99999-9999", "Modelo 01", "789456123", 500);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\nIPHONE:");
+Iphone iphone = new Iphone("98888-8888", "Modelo 02", "123456789", 256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
